1a0d4e35f4bc8117a757a6c72d0b9b92d1695343
Philipp Paul <mail@philipp-paul.de>
2014-01-15 11:33:00 +0100

    - Viele viele Änderrungen!

---
+++ app/controllers/RatingController.java
@@ -0,0 +1,11 @@
+package controllers;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: paul
+ * Date: 13.01.14
+ * Time: 16:00
+ * To change this template use File | Settings | File Templates.
+ */
+public class RatingController {
+}
